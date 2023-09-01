efec8d7ba19fcd9648b071c1746eaad3f405fcb8
CptMisagh <132880744+CptMisagh@users.noreply.github.com>
2023-09-01 12:12:54 +0330

    Add files via upload
    this is my exercise during my self-study learning Python.
    thank you for reading.

---
+++ sort.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+car = ['bmw', 'toyota', 'suzuki', 'kawazaki']
+print(car)
+car.sort()
+print(f"this is 'sort' list  :{car}")
+
+sorted(car)
+print(f"this is 'sorted' list  : {car}")
+
+
+
+
+
+
